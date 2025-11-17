057e6abd5b9ca5fe3317346e947341084dbe04f7
Lumikin <lumikomio02@gmail.com>
2025-11-17 16:21:54 -0300

    Comentando os codigos

--- src/model/clienteModel.js
+++ src/model/clienteModel.js
@@ -1,6 +1,28 @@
 const pool = require('../config/db');
 
 const clienteModel = {
+    // VERIFICADORES:
+    // CPF:
+    verificarCPF: async (consultarCPF) => {
+        const sql = 'SELECT * FROM clientes WHERE cpfCliente=?;';
+        const values = [consultarCPF];
+        const [rows] = await pool.query(sql, values);
+        console.log(rows);
+        return rows;
+    },
+
+    // EMAIL:
+
+    verificarEmail: async (consultarEmail) => {
+        const sql = 'SELECT * FROM clientes WHERE emailCliente=?;';
+        const values = [consultarEmail];
+        const [rows] = await pool.query(sql, values)
+        console.log(rows);
+        return rows;
+    },
+
+    // SELECIONAR:
+
     selecionarTodos: async () => {
         const sql = 'SELECT * FROM clientes;';
         const [rows] = await pool.query(sql)
@@ -12,6 +34,9 @@ const clienteModel = {
         const [rows] = await pool.query(sql, values)
         return rows;
     },
+
+    //INSERIR:
+
     inserirCliente: async (pNome, pCpf, pTel, pEmail, pEndereco) => {
         const sql = 'INSERT INTO clientes (nomeCliente, cpfCliente, telefoneCliente, emailCliente, enderecoCliente) VALUES (?,?,?,?,?);';
         const values = [pNome, pCpf, pTel, pEmail, pEndereco];
@@ -19,23 +44,7 @@ const clienteModel = {
         console.log(rows);
         return rows;
     },
-    // VERIFICADORES:
-    // CPF:
-    verificarCPF: async (consultarCPF) => {
-        const sql = 'SELECT * FROM clientes WHERE cpfCliente=?;';
-        const values = [consultarCPF];
-        const [rows] = await pool.query(sql, values);
-        console.log(rows);
-        return rows;
-    },
-    // EMAIL:
-    verificarEmail: async (consultarEmail) => {
-        const sql = 'SELECT * FROM clientes WHERE emailCliente=?;';
-        const values = [consultarEmail];
-        const [rows] = await pool.query(sql, values)
-        console.log(rows);
-        return rows;
-    }
+
 
 }
 

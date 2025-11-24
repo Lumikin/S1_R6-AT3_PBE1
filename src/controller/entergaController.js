1c0b250652d072e9d3bd7e549b3b06d1d38348ea
Lumikin <lumikomio02@gmail.com>
2025-11-24 15:42:18 -0300

    sdfghjk

--- src/controller/entergaController.js
+++ src/controller/entergaController.js
@@ -15,16 +15,28 @@ const entregaController = {
     adicionarEntregas: async (req, res) => {
         try {
             const { Pedido, Distancia, Peso, Acrescimo, Taxa, Desconto, Tipo, Status } = req.body;
-            if (!Distancia || !Peso || !Acrescimo || !Taxa || !Desconto || !Tipo || !Status || tipoEntrega === "normal" || tipoEntrega === "urgente" || valorDistancia < 0 || valorPeso < 0) {
+            if (!Pedido || !Distancia || !Peso || !Acrescimo || !Taxa || !Desconto || !Tipo || !Status || !tipoEntrega === "normal" || !tipoEntrega === "urgente" || valorDistancia < 0 || valorPeso < 0) {
                 return res.status(400).json({ message: 'Dados invalidos' })
             }
-            const resultado = await entregaModel.inserirEntrega(valorDistancia, valorPeso, acrescimo, taxaExtra, desconto, tipoEntrega, statusEntrega)
+
+            const ConsultarKm = entregaModel.consultarPrecoKM(Pedido)
+            if (ConsultarKm.length === 0) {
+                throw new Error(`Nao foi possivel consultar o preco em Km de ${Pedido}`);
+            }
+            const valorKm = ConsultarKm * Distancia
+            
+            const ConsultarKg = entregaModel.consultarPrecoKG(Pedido)
+            if (ConsultarKm.length === 0) {
+                throw new Error(`Nao foi possivel consultar o preco em Km de ${Pedido}`);
+            }
+            const valorKg = ConsultarKg * Peso
+
             if (resultado.affectedRows === 1 && resultado.insertId !== 0) {
                 res.status(201).json({ message: 'Registro incluido com sucesso', result: resultado })
             } else {
                 throw new Error('ocorreu um erro ao incluir o registro')
             }
-            const ValorFinal = {} 
+            const ValorFinal = {}
         } catch (error) {
             console.error(error);
             res.status(500).json({

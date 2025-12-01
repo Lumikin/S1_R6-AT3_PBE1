e811f41a8f5df99437dbb73567ef77de82b44ae2
Lumikin <lumikomio02@gmail.com>
2025-12-01 09:38:03 -0300

    add entrega routes

---
+++ src/routes/entregaRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const entregaRoutes = express.Router()
+const { entregaController } = require('../controller/entergaController');
+
+entregaRoutes.get('/Entregas', entregaController.listarEntregas);
+entregaRoutes.post('/Entregas/:idPedido', entregaController.adicionarEntregas);
+
+module.exports = { entregaRoutes }
+

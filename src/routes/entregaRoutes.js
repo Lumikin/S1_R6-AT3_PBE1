e8a0dbaef0398252d3c61c01512f2a93d3b80471
Lumikin <lumikomio02@gmail.com>
2025-12-01 09:35:06 -0300

    sdfjn

--- src/routes/entregaRoutes.js
+++ src/routes/entregaRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const entregaRoutes = express.Router()
 const { entregaController } = require('../controller/entergaController');
@@ -7,13 +6,4 @@ entregaRoutes.get('/Entregas', entregaController.listarEntregas);
 entregaRoutes.post('/Entregas/:idPedido', entregaController.adicionarEntregas);
 
 module.exports = { entregaRoutes }
-=======
-const express = require('express');
-const entregaRoutes = express.Router();
 
-const {entregaController} = require("../controller/entregaController");
-
-entregaRoutes.post('/entregas', entregaController.criarEntrega );
-
-module.exports = { entregaRoutes };
->>>>>>> 03743437a293e361870c595ff4ae4a13e11bdf94

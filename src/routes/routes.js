42d65e07ca11ee0d871808aa04650ad1f3859ea3
Lumikin <lumikomio02@gmail.com>
2025-12-01 09:40:58 -0300

    Arrumando os routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 //Referência do arquivo de rotas
+const { clentesRoutes } = require('./clienteRoutes')
 const { pedidoRoutes } = require('./pedidoRoutes')
 const { entregaRoutes } = require('./entregaRoutes')
 
 router.use('/', pedidoRoutes);
+router.use('/', clentesRoutes);
 router.use('/', entregaRoutes);
 
 module.exports = { router };

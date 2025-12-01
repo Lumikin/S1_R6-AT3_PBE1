217e45bfb5ac4b5cbb751bd8b5ac8385d70af899
Lumikin <lumikomio02@gmail.com>
2025-12-01 15:46:44 -0300

    Documentado Final

--- src/controller/entregaController.js
+++ src/controller/entregaController.js
@@ -88,50 +88,14 @@ const entregaController = {
         try {
             const fkPedido = Number(req.params.idPedido);
             const { status } = req.body;
-            // valida o ID do pedido
             if (!fkPedido || isNaN(fkPedido) || fkPedido <= 0) {
                 return res.status(400).json({
                     message: "Voce deve inserir um numero valido do pedido!!",
                 });
             }
-            if (rowsPedido.length === 0) {
-                throw new Error("Não foi possivel localizar o pedido")
+            if (status !== "entregue" && status !== "transitando" && status !== "cancelado") {
+                return res.status(400).json({ message: "Verifique se os status de entrega estao digitados: entregue, transitando ou cancelado", });
             }
-            const ConsultaPedido = entregaModel.consultarPedido(fkPedido)
-            const pedido = fkPedido.rowsPedido[0];
-
-            const valorDistancia = pedido.valorKm * pedido.distanciaPedido;
-            const valorPeso = pedido.valorKg * pedido.pesoCarga;
-            const valorBase = valorDistancia + valorPeso;
-            const tipoEntrega = pedido.tipoEntrega
-
-            // Cálculo do acréscimo baseado no tipo de entrega
-            let acrescimo = 0;
-            if (tipoEntrega == "urgente") {
-                acrescimo = valorBase * 0.2;
-            } else {
-                acrescimo = 0
-            }
-
-            const valorFinalParcial = valorBase + acrescimo;
-
-            // Cálculo do desconto para valores acima de R$ 500,00
-            let desconto = 0;
-            if (valorFinalParcial > 500) {
-                desconto = valorFinalParcial * 0.1;
-            }
-            // Cálculo da taxa extra para cargas acima de 50kg
-            let taxaExtra = 0;
-            if (pedido.pesoCarga > 50) {
-                taxaExtra = 15;
-            }
-
-            const valorFinal = valorFinalParcial - desconto + taxaExtra;
-            // valida o status da entrega
-            if (status !== "entregue" && status !== "transitando" && status !== "cancelado" && status !== "espera") {
-                return res.status(400).json({ message: "Verifique se os status de entrega estao digitados: entregue, transitando, espera ou cancelado", });
-            }
-            // incluir a nova entrega
             const resultado = await entregaModel.incluirEntregas(fkPedido, status);
             if (!resultado || resultado.length === 0) {
                 return res.status(200).json({ message: "Registro não encontrado!" })
